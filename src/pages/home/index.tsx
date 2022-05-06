aef01b64be1f03334eee523c8335d18baed27c61
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-05 19:30:01 -0500

    Added home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 function Home() {
-  return <div className={styles.homeContainer}></div>;
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.container}>
+        <div className={styles.title}>Welcome!</div>
+        <div className={styles.subtitle}>What would you like to do?</div>
+        <div className={styles.buttonGroup}>
+          <button className={styles.button}>Publish a pitch deck</button>
+          <button className={styles.button}>See available pitch decks</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

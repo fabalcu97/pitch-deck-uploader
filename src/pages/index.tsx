c1f86dce3345d3d1971b45f99981b9348c538af2
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-06 20:17:34 -0500

    Added react toastify and styles changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,23 @@ import Home from './home';
 import PublishPitchDeck from './publishPitchDeck';
 import ListPitchDecks from './listPitchDecks';
 import PitchDeck from './pitchDeck';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/publish-pitch-deck" element={<PublishPitchDeck />} />
-        <Route path="/list-pitch-decks" element={<ListPitchDecks />} />
-        <Route path="/pitch-deck/:id" element={<PitchDeck />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/publish-pitch-deck' element={<PublishPitchDeck />} />
+        <Route path='/list-pitch-decks' element={<ListPitchDecks />} />
+        <Route path='/pitch-deck/:id' element={<PitchDeck />} />
       </Routes>
+      <ToastContainer
+        position='top-center'
+        closeOnClick
+        closeButton={false}
+      />
     </BrowserRouter>
   );
 }

3f41df47b161a953279f6f80c881f5772c788c67
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-06 20:18:10 -0500

    Separated button to a component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,44 @@
+import { BaseComponentProps } from 'utils/types/baseComponent';
+import clsx from 'clsx';
+import React, { ButtonHTMLAttributes, useMemo } from 'react';
+import styles from './styles.module.scss';
+import { Link } from 'react-router-dom';
+
+type Props = {
+  label: string;
+  href?: string;
+  onClick?: () => void;
+  buttonProps?: ButtonHTMLAttributes<HTMLButtonElement>;
+  variant: 'primary' | 'secondary';
+} & BaseComponentProps;
+
+Button.defaultProps = {
+  variant: 'primary',
+};
+
+function Button(props: Props) {
+  const { containerStyle, label, onClick, href, variant, buttonProps } = props;
+  const isSecondary = useMemo(() => variant === 'secondary', [variant]);
+
+  return (
+    <div className={clsx([styles.buttonContainer, containerStyle])}>
+      {href ? (
+        <Link to={href}>
+          <div
+            className={clsx([styles.button, isSecondary && styles.secondary])}>
+            {label}
+          </div>
+        </Link>
+      ) : (
+        <button
+          className={clsx([styles.button, isSecondary && styles.secondary])}
+          onClick={onClick}
+          {...buttonProps}>
+          {label}
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Button;

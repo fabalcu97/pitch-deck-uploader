f9fa04a21f92ba1db7b40bc8eb7ccb7358540030
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-11 17:40:27 -0500

    Added loader to list

--- client/src/components/list/index.tsx
+++ client/src/components/list/index.tsx
@@ -4,37 +4,45 @@ import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
 import clsx from 'clsx';
 import { BaseComponentProps } from 'utils/types/baseComponent';
 
+import Loader from 'components/loader';
+
 import styles from './styles.module.scss';
 
 type Props<T> = {
   count: number;
   itemHeight: number;
   width?: number;
+  isLoading: boolean;
   RenderItem: ComponentType<ListChildComponentProps<T>>;
 } & BaseComponentProps;
 
 VirtualizedList.defaultProps = {
   itemHeight: 35,
+  isLoading: false,
 };
 
 function VirtualizedList<T>(props: Props<T>) {
-  const { containerStyle, RenderItem, count, itemHeight, width } = props;
+  const { containerStyle, RenderItem, count, isLoading, itemHeight, width } =
+    props;
 
   return (
     <div className={clsx([styles.listContainer, containerStyle])}>
-      <AutoSizer>
-        {({ height, width: autoSizerWidth }) => (
-          <List
-            height={height}
-            width={width ?? autoSizerWidth}
-            className={styles.list}
-            itemCount={count}
-            itemSize={itemHeight}
-            layout='vertical'>
-            {RenderItem}
-          </List>
-        )}
-      </AutoSizer>
+      <>
+        <AutoSizer>
+          {({ height, width: autoSizerWidth }) => (
+            <List
+              height={height}
+              width={width ?? autoSizerWidth}
+              className={styles.list}
+              itemCount={count}
+              itemSize={itemHeight}
+              layout='vertical'>
+              {RenderItem}
+            </List>
+          )}
+        </AutoSizer>
+        {isLoading && <Loader />}
+      </>
     </div>
   );
 }

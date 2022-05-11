afe3f809275332d136b40ccea3c218f696d6c36b
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-11 17:40:46 -0500

    Added react query and toast providers

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
@@ -10,18 +11,26 @@ import PublishPitchDeck from './publishPitchDeck';
 
 import 'react-toastify/dist/ReactToastify.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/publish-pitch-deck' element={<PublishPitchDeck />} />
-        <Route path='/list-pitch-decks' element={<ListPitchDecks />} />
-        <Route path='/pitch-deck/:id' element={<PitchDeck />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <ToastContainer position='top-center' closeOnClick closeButton={false} />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/publish-pitch-deck' element={<PublishPitchDeck />} />
+          <Route path='/list-pitch-decks' element={<ListPitchDecks />} />
+          <Route path='/pitch-deck/:id' element={<PitchDeck />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <ToastContainer
+          position='top-center'
+          closeOnClick
+          closeButton={false}
+        />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

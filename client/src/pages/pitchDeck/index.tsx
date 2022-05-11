2454fd879969bf68d011b6f1be8e6daa9deabe16
Fabricio Ballon <fballoncuadros@gmail.com>
2022-05-11 17:41:44 -0500

    Added lightbox to pitch deck page images

--- client/src/pages/pitchDeck/index.tsx
+++ client/src/pages/pitchDeck/index.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import clsx from 'clsx';
 import { format, parseISO } from 'date-fns';
-import { generatePitchDeck } from 'utils/factories/pitchDeck';
+import FsLightbox from 'fslightbox-react';
+import environment from 'utils/config/env';
+import useGetPitchDeck from 'utils/hooks/api/useGetPitchDeck';
 import { BaseComponentProps } from 'utils/types/baseComponent';
 import { PitchDeckType } from 'utils/types/pitchDeck';
 
 import Header from 'components/header';
+import Loader from 'components/loader';
 import Text from 'components/text';
 
 import styles from './styles.module.scss';
@@ -18,37 +22,86 @@ PitchDeck.defaultProps = {};
 function PitchDeck(props: Props) {
   const { containerStyle } = props;
   const [pitchDeck, setPitchDeck] = useState<PitchDeckType>();
-
+  const [images, setImages] = useState<string[]>([]);
   const { id } = useParams();
   const navigate = useNavigate();
+  const { data, error, isLoading } = useGetPitchDeck(id || '');
 
-  const getPitchDeck = (_id: string) => {
-    setPitchDeck(generatePitchDeck());
-  };
+  const [lightboxController, setLightboxController] = useState({
+    toggler: false,
+    slide: 0,
+  });
+
+  const openImageViewer = useCallback((index: number) => {
+    setLightboxController((value) => ({
+      toggler: !value.toggler,
+      slide: index + 1,
+    }));
+  }, []);
+
+  const Images = useMemo(
+    () =>
+      images.map((image: string, idx: number) => (
+        <img
+          key={idx}
+          className={styles.image}
+          alt=''
+          src={image}
+          onClick={() => openImageViewer(idx)}
+        />
+      )),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [images],
+  );
 
   useEffect(() => {
     if (!id) {
       navigate('/list-pitch-decks');
       return;
     }
-    getPitchDeck(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
+
+  useEffect(() => {
+    if (data) {
+      setPitchDeck(data);
+      setImages(
+        data.images.map((image) => `${environment.apiBaseUrl}${image}`),
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  useEffect(() => {
+    if (error) {
+      toast('Something went wrong, please try again');
+      console.error(error);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error]);
 
   return (
     <main className={clsx([styles.pitchDeckContainer, containerStyle])}>
       {!!pitchDeck && (
         <>
           <Header title={''} backButton />
-          <Text variant='title'>{pitchDeck.companyName}</Text>
-          <Text variant='body'>
-            Registration date:{' '}
-            {format(parseISO(pitchDeck.createdDate), 'do MMM, yyyy')}
-          </Text>
-          <div className={styles.images}>
-            {pitchDeck.images.map((image, idx) => (
-              <img key={idx} className={styles.image} alt='' src={image} />
-            ))}
-          </div>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <Text variant='title'>{pitchDeck.companyName}</Text>
+              <Text variant='body'>
+                Registration date:{' '}
+                {format(parseISO(pitchDeck.dateCreated), 'do MMM, yyyy')}
+              </Text>
+              <div className={styles.images}>{Images}</div>
+              <FsLightbox
+                toggler={lightboxController.toggler}
+                sources={images}
+                slide={lightboxController.slide}
+              />
+            </>
+          )}
         </>
       )}
     </main>
